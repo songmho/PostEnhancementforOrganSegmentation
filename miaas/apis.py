b175ddfa267a0fd7bf09eb938e387d75f8416732
Hanter Jung <hanterkr@gmail.com>
2016-02-24 10:46:17 +0900

    patient profile

--- miaas/apis.py
+++ miaas/apis.py
@@ -177,7 +177,7 @@ def handle_patient_profile_mgt(request):
             if not user_id:
                 raise Exception(MSG_INVALID_PARAMS)
             patient_profile = db.retrieve_patient_profile(user_id)
-            return JsonResponse(dict(constants.CODE_SUCCESS, **{'profile': patient_profile}))
+            return JsonResponse(dict(constants.CODE_SUCCESS, **{'profiles': patient_profile}))
 
         elif (request.method) == 'POST':
             # update patient profile
@@ -188,10 +188,15 @@ def handle_patient_profile_mgt(request):
                 raise Exception(MSG_INVALID_PARAMS)
             user_id = data['user_id']
             timestamp = data['timestamp']
-            for key, value in data['profiles'].items():
+            # for key, value in data['profiles'].items():
+            # logger.info(data)
+            for prof in data['profiles']:
+                # logger.info(prof)
+                key = prof['type']
+                value = prof['value']
                 if not db.add_patient_profile(user_id, key, value, timestamp):
                     raise Exception(MSG_PROFILE_FAILED)
-                # logger.info('userid=%s key=%s value=%s' % (user_id, key, value))
+                logger.info('userid=%s key=%s value=%s' % (user_id, key, value))
             return JsonResponse(constants.CODE_SUCCESS)
 
     except Exception as e:
@@ -211,7 +216,7 @@ def handle_physician_profile_mgt(request):
             if not user_id:
                 raise Exception(MSG_INVALID_PARAMS)
             physician_profile = db.retrieve_physician_profile(user_id)
-            return JsonResponse(dict(constants.CODE_SUCCESS, **{'profile': physician_profile}))
+            return JsonResponse(dict(constants.CODE_SUCCESS, **{'profiles': physician_profile}))
 
         elif (request.method) == 'POST':
             # update physician profile
@@ -235,7 +240,7 @@ def handle_physician_profile_mgt(request):
 
 @csrf_exempt
 def handle_medical_image_mgt(request):
-    db = cloud_db.DbManager();
+    db = cloud_db.DbManager()
     try:
         if(request.method) == 'GET':
             pass
@@ -248,7 +253,7 @@ def handle_medical_image_mgt(request):
 
 @csrf_exempt
 def handle_interpretation_mgt(request):
-    db = cloud_db.DbManager();
+    db = cloud_db.DbManager()
     try:
         if(request.method) == 'GET':
             pass
@@ -261,7 +266,7 @@ def handle_interpretation_mgt(request):
 
 @csrf_exempt
 def handle_analytics_mgt(request):
-    db = cloud_db.DbManager();
+    db = cloud_db.DbManager()
     try:
         if(request.method) == 'GET':
             pass
@@ -274,7 +279,7 @@ def handle_analytics_mgt(request):
 
 @csrf_exempt
 def handle_payment_mgt(request):
-    db = cloud_db.DbManager();
+    db = cloud_db.DbManager()
     try:
         if(request.method) == 'GET':
             pass
--- static/miass/js/profile.js
+++ static/miass/js/profile.js
@@ -2,6 +2,125 @@
  * Created by hanter on 2016. 2. 23..
  */
 
-$(document).ready(function() {
+var profiles = {};
 
+$(document).ready(function() {
+    $.ajax("api/patient_profile", {
+        method: 'GET',
+        data: {
+            user_id: user.user_id
+        },
+        dataType: 'json',
+        success: function(res) {
+            console.log(JSON.stringify(res));
+            if(res['code'] == 'SUCCESS') {
+                profiles = res['profiles'];
+                resetProfile();
+            } else {
+                openUpdateFailModal(res['msg'], 'Getting Profile Failed');
+            }
+        }
+    });
+
+    $('#btnFormReset').click(resetProfile);
+    $('#patientProfileForm').on('submit', function(e) {
+        e.preventDefault();
+
+        updateProfile();
+    });
 });
+
+
+function resetProfile() {
+    $('#patientProfileForm input, #patientProfileForm textarea').each(function() {
+        $(this).val('');
+    });
+
+    console.log(profiles);
+
+    for (var i in profiles) {
+        var profile = profiles[i];
+
+        if(profile.type == "height") {
+            var height = profile.value.split(" ");
+            $('#height').val(height[0]);
+            $('#heightType').val(height[1]).attr("selected", "selected");
+        } else if(profile.type == "weight") {
+            var weight = profile.value.split(" ");
+            $('#weight').val(weight[0]);
+            $('#weightType').val(weight[1]).attr("selected", "selected");
+        } else if(profile.type == "smoking") {
+            $('#smoking').val(profile.value).attr("selected", "selected");
+        } else {
+            $('#'+profile.type).val(profile.value);
+        }
+    }
+}
+
+var newProfiles = [];
+function updateProfile() {
+    newProfiles = [];
+
+    $('#patientProfileForm input, #patientProfileForm textarea').each(function() {
+        var id = $(this).attr('id');
+        var value = $(this).val();
+        var nowProf = {};
+
+        if (value == undefined || value == null || value == '' || value == ' ') {
+            return;
+        }
+
+        nowProf['type'] = id;
+        if (id == "height") {
+            nowProf['value'] = value + ' ' + $('#heightType').val()
+        } else if (id == "weight") {
+            nowProf['value'] = value + ' ' + $('#weightType').val()
+        } else {
+            nowProf['value'] = value;
+        }
+        newProfiles.push(nowProf);
+    });
+    newProfiles.push({type: 'smoking', value: $('#smoking').val()});
+
+    //console.log(newProfiles);
+    $.ajax("api/patient_profile", {
+        method: 'POST',
+        data: JSON.stringify({
+            user_id: 'hanter',
+            profiles: newProfiles,
+            timestamp: new Date().getTime()
+        }),
+        dataType: 'json',
+        success: function(res) {
+            console.log(JSON.stringify(res));
+            if(res['code'] = 'SUCCESS') {
+                profiles = newProfiles;
+                openUpdatedModal();
+            } else {
+                openUpdateFailModal(res['msg'], 'Update Failed');
+            }
+        }
+    });
+}
+
+function openUpdateFailModal(msg, title) {
+    if (title!=undefined && title!=null && title!='') {
+        $('#updateFailedTitle').text(title)
+    }
+    if (msg==undefined || msg==null || msg=='') {
+        msg = 'Updating failed. Please try again.'
+    }
+    $('#updateFailedModal .modal-body').text(msg);
+    $('#updateFailedModal').modal();
+}
+
+function openUpdatedModal(msg, title) {
+    if (title!=undefined && title!=null && title!='') {
+        $('#updatedTitle').text(title)
+    }
+    if (msg==undefined || msg==null || msg=='') {
+        msg = 'Your profile is successfully updated.'
+    }
+    $('#updatedModal .modal-body').text(msg);
+    $('#updatedModal').modal();
+}
