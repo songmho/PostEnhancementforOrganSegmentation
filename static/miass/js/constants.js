7c72d4e03b9ebdf6a5bddeec4848e4ac304f08ae
Hanter Jung <hanterkr@gmail.com>
2016-04-15 14:14:48 +0900

    Image Sequence Play control buttns

--- static/miass/js/constants.js
+++ static/miass/js/constants.js
@@ -2,5 +2,5 @@
  * Created by hanter on 2016. 4. 14..
  */
 
-//var SERVER_ADDRESS = 'http://localhost:8000';
-var SERVER_ADDRESS = 'http://203.253.23.26:80';
+var SERVER_ADDRESS = 'http://localhost:8000';
+//var SERVER_ADDRESS = 'http://203.253.23.26:80';
