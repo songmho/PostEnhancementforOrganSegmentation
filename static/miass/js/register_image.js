1d4b620290d42660b7e9a33570a3c1b0d0810c96
Jeonous <corea940@gmail.com>
2020-04-22 16:31:20 +0900

    update the the register_image.html and register_image.js for image folder upload

--- static/miass/js/register_image.js
+++ static/miass/js/register_image.js
@@ -1,3 +1,9 @@
 (function () {
-
+    function getfolder(e) {
+    var files = e.target.files;
+    var path = files[0].webkitRelativePath;
+    var Folder = path.split("/");
+    alert("Files in " + Folder[0] + " are uploaded.")
+}
 })(jQuery);
+
