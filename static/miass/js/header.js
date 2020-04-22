cd34d6e014b8029ba47291c6a82bd00b6742b66a
songmho <songmho@gmail.com>
2020-04-22 20:50:17 +0900

    add code to show current user info at right side after logined

--- static/miass/js/header.js
+++ static/miass/js/header.js
@@ -43,5 +43,50 @@
     $('#nav_main').click(function () {
         location.replace("main")
     });
+    $('#txt_role').click(function () {
+        c_u = get_current_user();
+        console.log("hi", c_u);
+         $.ajax("/api/signout", {
+            method: 'POST',
+            async: true,
+            data: JSON.stringify({
+                'email': c_u["email"],
+                'pwd': c_u["pwd"],
+                'identification_number':c_u["identification_number"]
+            }),
+            success: function(data) {
+                if (data['state'] === true) { // success to log in
+                    remove_current_user();
+                    location.reload();
+                }
+                else{
+
+                }
+            },
+             error: function (err) {
+                console.log(err);
+             }
+        });
+    });
+
+    $(document).ready(function () {
+        try {
+            c_u = get_current_user();
+            console.log(c_u);
+            if (c_u == null) {
+                $("#txt_role").prop("hidden", true);
+            } else {
+                $("#txt_role").prop("hidden", false);
+                var sen = c_u['first_name']+" "+c_u['last_name']+" ("+c_u["role"]+")";
+                console.log(sen);
+                $("#txt_role").last().html(sen);
+
+            }
+        } catch (e) {
+            console.log(c_u);
+            $("#txt_role").prop("hidden", true);
+        }
+
+    });
 
 })(jQuery);
