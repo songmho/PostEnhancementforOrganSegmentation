2999240bbcd4eb3eceef05e09c41a43cd40af99a
Jeonous <corea940@gmail.com>
2020-04-23 10:35:44 +0900

    modified the 2 files

--- static/miass/js/register_image.js
+++ static/miass/js/register_image.js
@@ -1,9 +1,23 @@
 (function () {
-    function getfolder(e) {
-        var files = e.target.files;
-        var path = files[0].webkitRelativePath;
-        var Folder = path.split("/");
-        alert("Files in " + Folder[0] + " are uploaded.")
-    }
+    // function getfolder(e) {
+    //     var files = e.target.files;
+    //     var path = files[0].webkitRelativePath;
+    //     var Folder = path.split("/");
+    //     alert("Files in " + Folder[0] + " are uploaded.")
+    // }
+
+    $("#btn_submit").click(function () {
+        var uploader_id = $("#uploader_id").val();
+        var taken_date = $("#taken_date").val();
+        var taken_place = $("#taken_place").val();
+        var image_path = $("#image_path").val();
+        // alert(uploader_id + taken_date + taken_place + image_path);
+        var folder_list = $("#image_folder").val();
+        alert(folder_list);
+    })
+
+    // $("#image_folder").
+
+
 })(jQuery);
 
