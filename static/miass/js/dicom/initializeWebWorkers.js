9960273c3a3f76a6465b6890ddc7fd66dcc01f35
songmho <songmho@gmail.com>
2020-05-27 09:19:54 +0900

    Revert "Change cornerstoneTool version as 2-> 4 Change functions following upgraded version"
    This reverts commit b760ef6f

--- static/miass/js/dicom/initializeWebWorkers.js
+++ static/miass/js/dicom/initializeWebWorkers.js
@@ -24,30 +24,6 @@ try {
   cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
   cornerstoneTools.external.cornerstone = cornerstone;
   cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
-  cornerstoneTools.init({
-    /**
-     * When cornerstone elements are enabled,
-     * should `mouse` input events be listened for?
-     */
-    mouseEnabled: true,
-    /**
-     * When cornerstone elements are enabled,
-     * should `touch` input events be listened for?
-     */
-    touchEnabled: true,
-    /**
-     * A special flag that synchronizes newly enabled cornerstone elements. When
-     * enabled, their active tools are set to reflect tools that have been
-     * activated with `setToolActive`.
-     */
-    globalToolSyncEnabled: false,
-    /**
-     * Most tools have an associated canvas or SVG cursor. Enabling this flag
-     * causes the cursor to be shown when the tool is active, bound to left
-     * click, and the user is hovering the enabledElement.
-     */
-    showSVGCursors: false,
-  });
   cornerstoneWADOImageLoader.webWorkerManager.initialize({
     maxWebWorkers: navigator.hardwareConcurrency || 1,
     startWebWorkersOnDemand: true,
@@ -60,8 +36,6 @@ try {
       }
     }
   });
-
-  cornerstoneTools.init();
 } catch (e) {
   // Already initialized
   console.log(e)
